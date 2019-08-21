6e3399efbe665a5ddee9ee4812db70938b0b7aaa
Akito Ito <akito_ito@r.recruit.co.jp>
2019-08-21 16:55:31 +0900

    update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,12 @@ module.exports = {
     project: "./tsconfig.json"
   },
   rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error", {
+      "vars": "all",
+      "args": "after-used",
+      "ignoreRestSiblings": false
+    }],
     "@typescript-eslint/adjacent-overload-signatures": "error"
   }
 };
